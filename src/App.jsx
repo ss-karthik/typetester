4c8ec420321883dc8b516ad3566e2e66f5e72322
ss-karthik <maniac.sskarthik@gmail.com>
2024-09-14 22:32:14 +0530

    Initial commit - basic wpm and accuracy functionality - 20 words

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tester from './Components/Tester'
+
+function App() {
+    return (
+    <>
+      <h1 className='text-7xl py-5'>TypeTester</h1>
+      <Tester />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Tester.jsx
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from 'react'
+import words from './Words';
+
+const Tester = () => {
+    const [typed, setTyped] = useState("");
+    const [remain, setRemain] = useState("")
+    const [errCount, setErrCount] = useState(0);
+    const [startTime, setStartTime] = useState(null);
+    const [wpm, setWpm] = useState(0);
+    const [acc, setAcc] = useState(0);
+
+    function handleChange(e) {
+        if(remain===""){
+            return;
+        }
+        if(typed===""){
+            const d = new Date();
+            setStartTime(d.getTime());
+        }
+        if(remain.length===1){
+            const d = new Date();
+            const time = (d.getTime() - startTime)/60000;
+            setWpm((20/time).toFixed(2));
+        }
+        
+        const key = e.key;
+        if(key.length === 1)
+        {
+            if(!remain.startsWith(key))
+                setErrCount(errCount+1);
+            setTyped((prevTyped)=>{
+                return prevTyped.replace('|', '') +remain[0]+"|";
+            });
+            setRemain((prevRemain)=>{
+                return prevRemain.substring(1);
+            });
+            remainingcont.textContent = text.substring(typed.length);
+        }
+    }
+    useEffect(()=>{
+        if(remain===""){
+            setAcc((100 - (errCount*100/typed.length)).toFixed(2));
+        } else {
+            setAcc(0);
+        }
+        document.addEventListener("keydown", handleChange);
+
+        return () => {
+            document.removeEventListener("keydown", handleChange);
+        }
+        
+    }, [typed, remain])
+    useEffect(()=>{
+        let text = "";
+        for(let i=0; i<20; i++){
+            let idx = Math.floor(Math.random() * (3103));
+            text+= " " + words[idx];
+        }
+        setRemain(()=>{
+            return text.trim();
+        });
+    }, [])
+  return (
+    <div>
+        <h1 className='text-4xl'>WPM: {wpm}</h1>
+        <h1 className='text-4xl'>Error Count: {errCount}</h1>
+        <h1 className='text-4xl'>Accuracy : {acc}</h1>
+      <div className='my-10'>
+      <span className='text-slate-500 text-4xl'>{typed}</span>
+      <span className='text-4xl'>{remain}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Tester
